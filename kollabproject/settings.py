2fc6fccfab61ca1d49e630f7803db10e0e0ebfc6
Imsha Nadeem <imshanadeem26@gmail.com>
2026-06-09 05:57:14 -0700

    Whitenoice statis handling adition

--- kollabproject/settings.py
+++ kollabproject/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← add this line here, second position
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -207,7 +208,7 @@ else:
     EMAIL_BACKEND      = "django.core.mail.backends.console.EmailBackend"
     DEFAULT_FROM_EMAIL = "KollabHub <noreply@kollabhub.com>"
 
-    
+
 # if os.environ.get("USE_REAL_EMAIL", "False").lower() in ("1", "true", "yes"):
 #     # ── Gmail SMTP (production) ─────────────────────────────────────
 #     EMAIL_BACKEND       = "django.core.mail.backends.smtp.EmailBackend"
@@ -244,6 +245,7 @@ USE_TZ        = True
 STATIC_URL       = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT      = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL  = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
